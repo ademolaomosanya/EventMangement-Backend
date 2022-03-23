69c9fcd19de6a3e2bc3fa6223b261805d3bf7b8b
ademola <omosanyaademola@gmail.com>
2022-03-23 22:58:37 +0800

    testing-API

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('myApp')
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World, from Ademola ");
+});
+
+app.listen(port, () =>
+    console.log(`Hello world app listening on port ${port}!`)
+);
